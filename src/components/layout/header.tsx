1386f53ab5795713f27c984cbcbe8a3f4ee3e760
Mehdi Mohammadi <59174850+mehmoh41@users.noreply.github.com>
2025-10-05 17:58:49 +0500

    Update header.tsx

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -74,8 +74,9 @@ export function Header() {
         </SheetContent>
       </Sheet>
       <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
-        <Button variant="ghost" size="icon" asChild>
+       
           <a
+            class="h-7 h-7 bg-transparent hover:bg-blue-500 p-4"
             href="https://github.com/mehmoh41/exoplanet-nasa-challenge"
             target="_blank"
             rel="noopener noreferrer"
@@ -84,7 +85,7 @@ export function Header() {
             <Github className="h-5 w-5" />
             <span className="sr-only">GitHub Repository</span>
           </a>
-        </Button>
+      
       </div>
     </header>
   );
